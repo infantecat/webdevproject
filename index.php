7a8ec844d56b18a517cf7e9f001947010e6d0ed5
infantecat <149574531+infantecat@users.noreply.github.com>
2023-11-01 20:24:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,24 +14,27 @@
 <body>
     <h1>My Subjects</h1>
     <a href="forms/addform.php">Add Subject</a><br/><br/>
-    <table width="25%" style="border: 1px solid black;">
-        <tr color='#CCCCCC'>
-            <td>ID</td>
-            <td>Subject Code</td>
-            <td>Subject Name</td>
-            <td>Action</td>
-        </tr>
-        <?php 
-            while($res = mysqli_fetch_assoc($result)) {
-                echo "<tr>";
-                echo "<td>".$res["subject_id"]."</td";
-                echo "<td>".$res["subject_code"]."</td";
-                echo "<td>".$res["subject_name"]."</td";
-                echo "<td> <a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> |
-                           <a href=\"functions/delete.php?id=$res[subject_id]\"
-                           onClick=\"return confirm ('Are you sure you want to delete?')\"Delete</a></td>";
-            }
-        ?>
-    </table>
+    <table width="100%" style="border: 1px solid black;">
+    <tr>
+        <th>ID</th>
+        <th>Subject Code</th>
+        <th>Subject Name</th>
+        <th>Action</th>
+    </tr>
+    <?php 
+        while ($res = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td align='center'>".$res["subject_id"]."</td>";
+            echo "<td align='center'>".$res["subject_code"]."</td>";
+            echo "<td align='center'>".$res["subject_name"]."</td>";
+            echo "<td align='center'>
+                      <a href=\"forms/editform.php?id=$res[subject_id]\">Edit</a> |
+                      <a href=\"functions/delete.php?id=$res[subject_id]\"
+                      onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a>
+                  </td>";
+        }
+    ?>
+</table>
+
 </body>
 </html>
